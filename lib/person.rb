f41df324d0b1a8cec52a5e5e07e74387008aab33
NiketaJen <niketa.jenerette@gmail.com>
2020-06-03 21:21:05 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def job=(work)
+        @job= work
+    end
+
+    def job
+        return @job
+    end
+
+    def name=(person_name)
+        @name= person_name
+    end
+
+    def name
+        return @name
+    end
+end
